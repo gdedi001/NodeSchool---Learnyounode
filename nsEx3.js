0ee6f411fa1562472ef2312ef60f5059cd9ce72a
GabrielD <gdedi651@gmail.com>
2015-08-11 01:10:43 -0400

    solution to exercise 3

--- nsEx3.js
+++ nsEx3.js
@@ -1,1 +1,31 @@
-var fs
+/*
+
+ MY FIRST I/O!
+ Exercise 3 of 13
+
+Write a program that uses a single synchronous filesystem operation to read a file and print the number of newlines (\n) it contains 
+to the console (stdout), similar to running cat file | wc -l.
+
+The full path to the file to read will be provided as the first command-line argument. You do not need to make your own test file.
+
+*/
+
+var fs = require('fs');
+
+var fileName = process.argv[2];
+
+//console.log(process.argv); see the contents of the nodeschool's provided argument
+
+var contents = fs.readFileSync(fileName); // This method will return a Buffer object containing the complete contents of the file.
+
+/* 
+Buffer objects are Node's way of efficiently representing arbitrary arrays of data, whether it be ascii, binary 
+or some other format. Buffer objects can be converted to strings by simply calling the toString() method on them.
+*/
+
+contents = contents.toString();
+
+console.log(contents.split("\n").length-1);
+
+
+
